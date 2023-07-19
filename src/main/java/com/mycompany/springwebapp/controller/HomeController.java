bb35c88701190d5b69f3e945373bcb6f2c8c884b
noismik <www_lol@naver.com>
2023-07-19 10:34:50 +0900

    이미지 추가

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.mycompany.springwebapp.controller;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,16 +11,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
-	//http://localhost:8080/springwebapp
+	
+	public HomeController() {
+		log.info("실행");
+		
+		
+		
+	}
+	
+	@PostConstruct
+	public void method1() {
+		log.info("실행");
+	}
+	@PostConstruct
+	public void method2() {
+		log.info("실행");
+	}
+	
+	@PreDestroy
+	public void method3() {
+		log.info("실행");
+		
+		
+	}
+	//http://localhost:8080/springwebapp/
 	@RequestMapping("/")
 	public String index() {
 		
 		log.info("실행1");
-<<<<<<< HEAD
 		log.info("실행2");
-=======
-
->>>>>>> branch 'master' of https://github.com/noismik/springwebapp.git
 		return "index";
 		
 		

97ba2a9341bd8c3157d1ec9a1dbbf4d07fb8f320
noismik <www_lol@naver.com>
2023-07-24 17:58:09 +0900

    Ch08Member 클래스 추가

---
+++ src/main/java/com/mycompany/springwebapp/dto/Ch08Member.java
@@ -0,0 +1,14 @@
+package com.mycompany.springwebapp.dto;
+
+import lombok.Data;
+
+@Data
+public class Ch08Member {
+
+	private String mid;
+	private String mname;
+	private String mpassword;
+	private String mtel;
+	private String maddress;
+	
+}

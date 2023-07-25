b38962a167581f4b21545deb4ba2d350b4cdbc93
noismik <www_lol@naver.com>
2023-07-25 18:14:33 +0900

    exception commit

---
+++ src/main/java/com/mycompany/springwebapp/exception/Ch10SoldOutException.java
@@ -0,0 +1,12 @@
+package com.mycompany.springwebapp.exception;
+
+public class Ch10SoldOutException extends RuntimeException {
+	public Ch10SoldOutException() {
+		
+	}
+	public Ch10SoldOutException(String message) {
+		super(message);
+		
+	}
+
+}

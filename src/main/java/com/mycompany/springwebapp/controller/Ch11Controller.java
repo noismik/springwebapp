d84e91a93b7a6e33491d5d368aec4e1f3ff9065b
noismik <www_lol@naver.com>
2023-07-26 17:58:50 +0900

    0726수정 사항

--- src/main/java/com/mycompany/springwebapp/controller/Ch11Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch11Controller.java
@@ -1,14 +1,20 @@
 package com.mycompany.springwebapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.springwebapp.dto.Ch11City;
 import com.mycompany.springwebapp.dto.Ch11Member;
+import com.mycompany.springwebapp.dto.Ch11Skill;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,5 +42,113 @@ public class Ch11Controller {
 	   return "redirect:/ch11/content";
 	   
    }
+   
+   @GetMapping("/form2")
+   public String form2(@ModelAttribute("member") Ch11Member member, Model model) {
+	   
+	   //드롭다운리스트의 항목을 추가할 목적
+	   List<String> typeList = new ArrayList<>();
+	   typeList.add("일반회원");
+	   typeList.add("기업회원");
+	   typeList.add("헤드헌터회원");
+	   model.addAttribute("typeList", typeList);
+	   //기본값을 설정
+	   member.setMtype("헤드헌터회원");
+	   
+	   List<String> jobList = new ArrayList<>();
+	   jobList.add("학생");
+	   jobList.add("개발자");
+	   jobList.add("디자이너");
+	   model.addAttribute("jobList", jobList);
+	   
+	   List<Ch11City> cityList = new ArrayList<>();
+	   cityList.add(new Ch11City(1, "서울"));
+	   cityList.add(new Ch11City(2, "부산"));
+	   cityList.add(new Ch11City(3, "제주"));
+	   model.addAttribute("cityList", cityList);
+	   
+	   member.setMcity(3);
+	   
+	   return "ch11/form2";
+	   
+   }
+   
+   @PostMapping("/form2")
+   public String form2(@ModelAttribute("member") Ch11Member member) {
+      log.info(member.toString());
+      return "redirect:/ch11/content";
+   }
+   
+   @GetMapping("/form3")
+   public String form3(@ModelAttribute("member") Ch11Member member, Model model) {
+	   List<String> languageList = new ArrayList<>();
+	   languageList.add("C");
+	   languageList.add("Pythonn");
+	   languageList.add("Java");
+	   languageList.add("JavaScript");
+	   languageList.add("HTML");
+	   model.addAttribute("languageList", languageList);
+	   member.setMlanguage(new String[] {"Java", "HTML"});
+	   
+	   List<Ch11Skill> skillList = new ArrayList<>();
+	   skillList.add(new Ch11Skill(1, "SpringFramework"));
+	   skillList.add(new Ch11Skill(2, "Boorstrap"));
+	   skillList.add(new Ch11Skill(1, "MyBatis"));
+	   model.addAttribute("skillList", skillList);
+	   member.setMskill(new int[] {1, 2});
+	   return "ch11/form3";
+   }
+   
+   @PostMapping("/form3")
+   public String form3(@ModelAttribute("member") Ch11Member member) {
+      log.info(member.toString());
+      return "redirect:/ch11/content";
+   }
+   
+   @GetMapping("/form4")
+   public String form4(@ModelAttribute("member") Ch11Member member, Model model) {
+	   List<String> jobList = new ArrayList<>();
+	   jobList.add("백수");
+	   jobList.add("재벌");
+	   jobList.add("개발자");
+	   jobList.add("물리치료사");
+	   jobList.add("의사");
+	   model.addAttribute("jobList", jobList);
+	   
+
+	   member.setMjob("백수");
+	   
+	   List<Ch11City> cityList = new ArrayList<>();
+	   cityList.add(new Ch11City(1, "서울"));
+	   cityList.add(new Ch11City(2, "부산"));
+	   cityList.add(new Ch11City(3, "제주"));
+	   model.addAttribute("cityList", cityList);
+	   
+	   member.setMcity(3);
+	   
+	   return "ch11/form4";
+	   
+	   
+	   
+	   
+   }
+   
+   @PostMapping("/form4")
+   public String form4(@ModelAttribute("member") Ch11Member member) {
+	   log.info(member.toString());
+	   return "redirect:/ch11/content";
+   }
+   
+   @GetMapping("/form5")
+   public String form5(@ModelAttribute("member")  Ch11Member member) {
+	   log.info(member.toString());
+	   
+	   return "ch11/form5";
+	   
+	   
+	   
+	   
+   }
+
    
 }

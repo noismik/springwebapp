b6cef37c28601d9558de8069d6a945c0e96af89c
noismik <www_lol@naver.com>
2023-07-27 20:04:25 +0900

    0727수정본

--- src/main/java/com/mycompany/springwebapp/controller/Ch12Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch12Controller.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.springwebapp.service.Ch12ServiceDiByAnnotation;
 import com.mycompany.springwebapp.service.Ch12ServiceDiByXml;
+import com.mycompany.springwebapp.service.Ch12ServicePropertyDi;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,10 +20,18 @@ public class Ch12Controller {
 	@Autowired
 	private Ch12ServiceDiByXml serviceDiByXml;
 	
+	@Autowired
+	private Ch12ServiceDiByAnnotation serviceDiByAnnotation;
+	
+	@Resource
+	private Ch12ServicePropertyDi servicePropertyDi;
+	
 	
    @RequestMapping("/content")
    public String content(HttpSession session) {
 	   serviceDiByXml.method();
+	   serviceDiByAnnotation.method();
+	   servicePropertyDi.method();
       return "ch12/content";
    }
    
